632efb342f1ec8d4524fe32a965ade302e99778f
Glenn West <glennswest@neuralcloudcomputing.com>
2017-05-10 07:23:52 +0800

    Add edit and create

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@
 import React from 'react';
 import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 
-import { PostList } from './posts';
+import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
 
 const App = () => (
     <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
-        <Resource name="posts" list={PostList} />
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
         <Resource name="users" list={UserList} />
     </Admin>
 );
